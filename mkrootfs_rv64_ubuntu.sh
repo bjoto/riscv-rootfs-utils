c64917d83e1226d0fb09546179bfbe438649bb25
Björn Töpel <bjorn@rivosinc.com>
2024-03-04 17:40:05 +0100

    Add more packages to rootfs
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>

--- mkrootfs_rv64_ubuntu.sh
+++ mkrootfs_rv64_ubuntu.sh
@@ -10,9 +10,42 @@ d=$(dirname "${BASH_SOURCE[0]}")
 distro=${1:-mantic}
 
 packages=(
-    linux-image-generic
+    automake
+    bc
+    bison
     build-essential
+    build-essential
+    bzip2
+    ca-certificates
+    cpio
+    debianutils
+    fakeroot
+    flex
+    gawk
+    gcc
+    git
+    gzip
+    kmod
+    libarchive-tools
+    libc6-dev
+    libipc-run-perl
+    libklibc-dev
+    libssl-dev
+    libtool
+    linux-image-generic
+    linux-libc-dev
+    make
+    numactl
+    openssl
+    patch
+    python-is-python3
+    rsync
+    rsync
+    ruby
+    ruby-dev
     systemd-boot-efi
+    time
+    wget
 )
 packages=$(IFS=, && echo "${packages[*]}")
 
